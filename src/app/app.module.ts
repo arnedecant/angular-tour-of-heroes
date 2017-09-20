9952ec9b4280d04eeee16d5ecb4430c7a6465b57
Arne Decant <arne@ris.be>
2017-09-20 16:45:25 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService }         from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeroDetailComponent,
+		HeroesComponent,
+		DashboardComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		AppRoutingModule
+	],
+	providers: [
+		HeroService
+	],
+	bootstrap: [
+		AppComponent
+	]
 })
+
 export class AppModule { }

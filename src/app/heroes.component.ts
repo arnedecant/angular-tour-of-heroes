01a02744f74c79c099751bb96d9c7c61d3f9825e
Arne Decant <arne@ris.be>
2017-09-21 12:26:41 +0200

    finished tutorial

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -15,23 +15,39 @@ export class HeroesComponent implements OnInit {
 	heroes: Hero[];
 	selectedHero: Hero;
 
+	constructor(private heroService: HeroService, private router: Router) {}
+
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 	}
 
 	getHeroes(): void {
-	  this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
 	}
 
 	gotoDetail(): void {
-	  this.router.navigate(['/detail', this.selectedHero.id]);
+		this.router.navigate(['/detail', this.selectedHero.id]);
 	}
 
-	constructor(private heroService: HeroService, private router: Router) {
+	add(name: string): void {
+		name = name.trim();
+		if (!name) { return; }
+		this.heroService.create(name)
+		.then(hero => {
+			this.heroes.push(hero);
+			this.selectedHero = null;
+		});
+	}
 
+	delete(hero: Hero): void {
+		this.heroService.delete(hero.id)
+		.then(() => {
+			this.heroes = this.heroes.filter(h => h !== hero);
+			if (this.selectedHero === hero) { this.selectedHero = null; }
+		});
 	}
 
 	ngOnInit(): void {
 		this.getHeroes();
-  	}
+	}
 }
